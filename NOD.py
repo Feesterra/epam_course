66419154fb6d9117478b79a23533c9cfce4abb53
Feesterra <33396740+Feesterra@users.noreply.github.com>
2018-03-26 16:27:31 +0300

    Update NOD.py

--- NOD.py
+++ NOD.py
@@ -5,14 +5,14 @@ while True:
     a = int(input())
     b = int(input())
     if a <= b:
-        minimal = a
+        minimal = abs(a)
     else:
-        minimal = b
+        minimal = abs(b)
     if minimal != 0:
-        for i in range(1, abs(minimal)+1):
+        for i in range(1, minimal+1):
             if (a % i == 0) & (b % i == 0):
                 nod = i
-        print(nod)
         break
     else:
         print('Division by zero is not defined. Please input other integers next time.')
+print(nod)
